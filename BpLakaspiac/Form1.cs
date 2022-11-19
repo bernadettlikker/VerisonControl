c392056d3ff9a29984a3951df5c5303677a9ab05
wk06gw <wk06gw@SZA0030913>
2022-11-19 16:47:25 +0100

    adatok betoltese

--- BpLakaspiac/Form1.cs
+++ BpLakaspiac/Form1.cs
@@ -12,9 +12,20 @@ namespace BpLakaspiac
 {
     public partial class Form1 : Form
     {
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities();
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
+
+        private void LoadData()
+        {
+            Flats = context.Flat.ToList();
+        }
+
+
     }
 }
