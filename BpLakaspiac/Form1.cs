bf52f0399f3ede6a833cce1ee0cb750312f3e5ef
wk06gw <wk06gw@SZA0030913>
2022-11-19 17:24:57 +0100

    create table

--- BpLakaspiac/Form1.cs
+++ BpLakaspiac/Form1.cs
@@ -54,13 +54,47 @@ namespace BpLakaspiac
 
 
         }
-
-        
-
-        void LoadData()
+        private void CreateTable()
         {
-            Flats = re.Flat.ToList();
-        }
+            string[] headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 1; i < headers.Length; i++)
+                xlSheet.Cells[1, i] = headers[i - 1];
+
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+            foreach (Flat item in Flats)
+            {
+                values[counter, 0] = item.Code;
+                values[counter, 1] = item.Vendor;
+                values[counter, 2] = item.Side;
+                values[counter, 3] = item.District;
+                values[counter, 4] = item.Elevator;
+                values[counter, 5] = item.NumberOfRooms;
+                values[counter, 6] = item.FloorArea;
+                values[counter, 7] = item.Price;
+                values[counter, 8] = "=" + GetCell(counter + 2, 8) + "*1000000/" + GetCell(counter + 2, 7);
+                counter++;
+
+            }
+
+
+
+            void LoadData()
+            {
+                Flats = re.Flat.ToList();
+            }
 
         
     }
