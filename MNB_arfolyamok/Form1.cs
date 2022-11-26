e4fd70652727077fc3a073b06b9f062729e65324
wk06gw <wk06gw@SZA0030913>
2022-11-26 09:16:53 +0100

    webszolg hivas

--- MNB_arfolyamok/Form1.cs
+++ MNB_arfolyamok/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MNB_arfolyamok.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,18 @@ namespace MNB_arfolyamok
         public Form1()
         {
             InitializeComponent();
+
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody() //új példányt hoztunk létre
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            GetExchangeRatesRequestBody response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangesRateResult;
+            //MessageBox.Show(result);
         }
     }
 }
