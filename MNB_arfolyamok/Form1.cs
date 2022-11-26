ff2294c88dcc456b83634dc5fba99e93b601a994
wk06gw <wk06gw@SZA0030913>
2022-11-26 10:12:36 +0100

    5. XML feldolgozása

--- MNB_arfolyamok/Form1.cs
+++ MNB_arfolyamok/Form1.cs
@@ -1,4 +1,5 @@
-﻿using MNB_arfolyamok.MnbServiceReference;
+﻿using MNB_arfolyamok.Entities;
+using MNB_arfolyamok.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,15 +9,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace MNB_arfolyamok
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Rates;
 
+            //GetRates();
+
+            ReadXml();
+        }
+
+        private void ReadXml()
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(GetRates());
+            foreach (XmlElement item in xml.DocumentElement) //végigmegy az xml minden elemén
+            {
+                RateData rd = new RateData();
+                Rates.Add(rd);
+                rd.Currency = item.ChildNodes[0].Attributes["curr"].Value; //childenodes-zal lejebb ugrik egyet
+                rd.Date = Convert.ToDateTime(item.Attributes["date"].Value); //item date attribútúmát kiolvassa és konvertálja dátummá
+                decimal unit = Convert.ToDecimal(item.ChildNodes[0].Attributes["unit"].Value);
+                decimal value = Convert.ToDecimal(item.ChildNodes[0].InnerText);
+                if (unit != 0)
+                {
+                    rd.Value = value / unit;
+                }
+                else { rd.Value = value; }
+            }
+        }
+
+        private static string GetRates() //a string eredetileg void volt, eljárásból függvény lesz (+return lent, + xml.Loadxml...)
+        {
             MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
             GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody() //új példányt hoztunk létre
             {
@@ -25,9 +57,10 @@ namespace MNB_arfolyamok
                 endDate = "2020-06-30"
             };
 
-            GetExchangeRatesRequestBody response = mnbService.GetExchangeRates(request);
-            string result = response.GetExchangesRateResult;
-            //MessageBox.Show(result);
+            GetExchangeRatesResponseBody response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+            MessageBox.Show(result);
+            return result;
         }
     }
 }
